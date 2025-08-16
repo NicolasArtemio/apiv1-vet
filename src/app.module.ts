5b917647654692286c2610c3f67d744d24a5e48f
NicolasArtemio <nicolasartemiodume@gmail.com>
2025-08-16 15:08:04 -0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,8 @@
-
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
-import { User } from './users/entities/user.entity';
-import { AuthModule } from './auth/auth.module';
-
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'admin',
-      database: 'db_veterinaria',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-    UsersModule,
-    AuthModule,
-  ],
+  imports: [],
+  controllers: [],
+  providers: [],
 })
-export class AppModule { }
+export class AppModule {}
