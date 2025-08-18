4a5a0199603fd921f5722f206fefa5833d8c88d7
NicolasArtemio <nicolasartemiodume@gmail.com>
2025-08-18 19:12:34 -0300

    se agrego archivo env de ejemplo, y se configuro la conexion a la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.getOrThrow<string>('DB_PORT'), 10),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true, // ¡no usar en producción!
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
