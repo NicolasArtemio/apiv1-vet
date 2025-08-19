37fe749cd07a7c735c2152f6ff07acfe591ab40d
marina <mari68253@gmail.com>
2025-08-19 20:19:40 -0300

    actualizando

--- src/notificaciones/notificaciones.module.ts
+++ src/notificaciones/notificaciones.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificacionesService } from './notificaciones.service';
 import { NotificacionesController } from './notificaciones.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Notificacion } from './entities/notificacione.entity';
 
 @Module({
+     imports: [TypeOrmModule.forFeature([Notificacion])],
   controllers: [NotificacionesController],
   providers: [NotificacionesService],
 })
