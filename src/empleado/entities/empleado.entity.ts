9f118960d016c3617ffea5e58f6c8c28625a3405
Jonathan <padinjonathan1@gmail.com>
2025-08-19 13:20:14 -0300

    Creadas entidades y DTOs para Cliente, Usuario y Empleado.

--- src/empleado/entities/empleado.entity.ts
+++ src/empleado/entities/empleado.entity.ts
@@ -1,1 +1,31 @@
-export class Empleado {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Empleado {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar', { length: 255 })
+  nombre: string;
+
+  @Column('varchar', { length: 255 })
+  apellido: string;
+
+  @Column('timestamp')
+  fecha_nacimiento: Date;
+  
+  @Column('int')
+  dni: number;
+
+  @Column('varchar', { length: 15 })
+  telefono: string;
+
+  @Column('varchar', { length: 255 })
+  ciudad: string;
+
+  @Column('varchar', { length: 255 })
+  direccion: string;
+
+  @Column('varchar', { length: 255 })
+  especialidad: string;
+}
