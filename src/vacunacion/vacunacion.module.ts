643315289525bd068c7d4ec1a785a95fa64af88f
cele_ruspil <celesteruspil22@gmail.com>
2025-08-20 21:35:44 -0300

    Conexion a la db

--- src/vacunacion/vacunacion.module.ts
+++ src/vacunacion/vacunacion.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { VacunacionService } from './vacunacion.service';
 import { VacunacionController } from './vacunacion.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Vacunacion } from './entities/vacunacion.entity';
 
-@Module({
+@Module({ imports: [TypeOrmModule.forFeature([Vacunacion])],
   controllers: [VacunacionController],
   providers: [VacunacionService],
 })
