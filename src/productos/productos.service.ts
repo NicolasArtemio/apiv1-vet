00d7536a6d628d805e6d029e20acb3a6882c3d45
cele_ruspil <celesteruspil22@gmail.com>
2025-09-01 20:23:37 -0300

    inicie crud de producto

--- src/productos/productos.service.ts
+++ src/productos/productos.service.ts
@@ -1,12 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateProductoDto } from './dto/create-producto.dto';
 import { UpdateProductoDto } from './dto/update-producto.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Producto } from './entities/producto.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductosService {
-  create(createProductoDto: CreateProductoDto) {
-    return 'This action adds a new producto';
+  constructor(
+    @InjectRepository(Producto)
+    private readonly productosRepository: Repository<Producto>
+  ) { }
+
+  async create(createProductoDto: CreateProductoDto) {
+    try {
+      const producto = this.productosRepository.create(createProductoDto)
+
+      return await this.productosRepository.save(producto);
+    } catch (error) {
+
+      console.error('Error mientras se crea el producto', error);
+      if (typeof error === 'object' && error !== null) {
+        const err = error as { code?: string; errno?: number };
+
+        if (err.code === 'ER_DUP_ENTRY' || err.errno === 1062) {
+          throw new ConflictException(
+            'Ya existe un producto con datos duplicados',
+          );
+        }
+        if (err.code === 'ER_NO_REFERENCED_ROW_2' || err.errno === 1452) {
+          throw new BadRequestException('Datos referenciados no existen');
+        }
+      }
+      throw new InternalServerErrorException('Error al crear producto');
+    }
+
   }
+
 
   findAll() {
     return `This action returns all productos`;
