d8d479df482f270e6022456f9e7b361925cb7a04
cele_ruspil <celesteruspil22@gmail.com>
2025-09-16 16:57:41 -0300

    testing de Clienteservice CRUD, falta agregar capturar excepcioones

--- src/cliente/cliente.service.spec.ts
+++ src/cliente/cliente.service.spec.ts
@@ -5,11 +5,12 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { Cliente } from './entities/cliente.entity';
 import { UsuarioService } from '../usuario/usuario.service';
 import { Usuario } from '../usuario/entities/usuario.entity';
+import { UpdateClienteDto } from './dto/update-cliente.dto';
 
 describe('ClienteService', () => {
   let service: ClienteService;
-  let serviceUsuario : UsuarioService;
-  
+  let serviceUsuario: UsuarioService;
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -27,10 +28,154 @@ describe('ClienteService', () => {
 
   it('deberia estar el servicioUsuario definido', () => {
     expect(serviceUsuario).toBeDefined();
-     });
-  
+  });
+
 
   it('deberia estar el servicioCliente definido', () => {
     expect(service).toBeDefined();
   });
+
+  describe('create', () => {
+    it('deberia crear un cliente', async () => {
+      const ClienteDto = {
+        foto_perfil: 'ruta/a/foto.jpg',
+        nombre: 'Juan',
+        apellido: 'Perez',
+        fecha_nacimiento: new Date('1990-01-01'),
+        dni: 12345678,
+        telefono: '123456789',
+        ciudad: 'Olavarria',
+        direccion: 'Calle Falsa 123',
+        email: 'juanperez@gmail.com',
+        contrasena: 'password123'
+      };
+      const resultadoEsperado = { id: 1, ...ClienteDto }
+      mockRepositoryCliente.create.mockReturnValue(resultadoEsperado);
+      mockRepositoryCliente.save.mockResolvedValue(resultadoEsperado);
+
+      const resultado = await service.create(ClienteDto);
+      expect(mockRepositoryCliente.create).toHaveBeenCalledWith(ClienteDto);
+      expect(mockRepositoryCliente.save).toHaveBeenCalledWith(resultadoEsperado);
+      expect(resultado).toEqual(resultadoEsperado);
+
+    })
+  });
+  describe('findAll', () => {
+    it('deberia que devolver un array de clientes', async () => {
+      const resultadoEsperado = [
+        {
+          id: 1,
+          foto_perfil: 'ruta/a/foto.jpg',
+          nombre: 'Juan',
+          apellido: 'Perez',
+          fecha_nacimiento: new Date('1990-01-01'),
+          dni: 12345678,
+          telefono: '123456789',
+          ciudad: 'Olavarria',
+          direccion: 'Calle Falsa 123',
+          email: 'juanperez@gmail.com',
+          contrasena: 'password123'
+        },
+        {
+          id: 2,
+          foto_perfil: 'ruta/a/foto.jpg',
+          nombre: 'Maria',
+          apellido: 'Perez',
+          fecha_nacimiento: new Date('1990-08-02'),
+          dni: 12342134,
+          telefono: '123412358',
+          ciudad: 'Olavarria',
+          direccion: 'Calle Falsa 789',
+          email: 'mariaperez@gmail.com',
+          contrasena: 'password123'
+        },
+        {
+          id: 3,
+          foto_perfil: 'ruta/a/foto.jpg',
+          nombre: 'Juliana',
+          apellido: 'Perez',
+          fecha_nacimiento: new Date('1990-01-18'),
+          dni: 78945612,
+          telefono: '2284696965',
+          ciudad: 'Olavarria',
+          direccion: 'Calle Falsa 123',
+          email: 'julianaperez@gmail.com',
+          contrasena: 'password123'
+        }
+      ]
+      mockRepositoryCliente.find.mockResolvedValue(resultadoEsperado);
+
+      const resultado = await service.findAll();
+      expect(mockRepositoryCliente.find).toHaveBeenCalled();
+      expect(resultado).toEqual(resultadoEsperado);
+    })
+  })
+
+  describe('findOne', () => {
+    it('deberia devolver un cliente por id', async () => {
+      const id = 1;
+      const resultadoEsperado = {
+        id: 1,
+        foto_perfil: 'ruta/a/foto.jpg',
+        nombre: 'Juan',
+        apellido: 'Perez',
+        fecha_nacimiento: new Date('1990-01-01'),
+        dni: 12345678,
+        telefono: '123456789',
+        ciudad: 'Olavarria',
+        direccion: 'Calle Falsa 123',
+        email: 'juanperez@gmail.com'
+      }
+      mockRepositoryCliente.findOneBy.mockResolvedValue(resultadoEsperado);
+
+      const resultado = await service.findOne(id);
+      expect(mockRepositoryCliente.findOneBy).toHaveBeenCalledWith({ id });
+      expect(resultado).toEqual(resultadoEsperado);
+    })
+  })
+
+  describe('update', () => {
+    it('deberia actualizar un cliente por id', async () => {
+      const id = 1;
+      const updateClienteDto = {
+        foto_perfil: 'ruta/a/foto.jpg',
+        nombre: 'Juan',
+        apellido: 'Perez',
+        fecha_nacimiento: new Date('1990-01-01'),
+        dni: 12345678,
+        telefono: '123456789',
+        ciudad: 'Olavarria',
+        direccion: 'Calle Falsa 123',
+        email: 'juanperez@gmail.com'
+      }
+
+      const resultadoEsperado = { id: 1, ...updateClienteDto }
+      mockRepositoryCliente.create.mockReturnValue(resultadoEsperado);
+      mockRepositoryCliente.save.mockResolvedValue(resultadoEsperado);
+    })
+  })
+
+  describe('remove', () => {
+    it('deberia eliminar un cliente por id', async () => {
+      const id = 1;
+      const resultadoEsperado = {
+        id: 1,
+        foto_perfil: 'ruta/a/foto.jpg',
+        nombre: 'Juan',
+        apellido: 'Perez',
+        fecha_nacimiento: new Date('1990-01-01'),
+        dni: 12345678,
+        telefono: '123456789',
+        ciudad: 'Olavarria',
+        direccion: 'Calle Falsa 123',
+        email: 'juanperez@gmail.com' }
+
+        mockRepositoryCliente.findOneBy.mockResolvedValue(resultadoEsperado);
+        mockRepositoryCliente.remove.mockResolvedValue(resultadoEsperado);  
+        const resultado = await service.remove(id);
+        expect(mockRepositoryCliente.findOneBy).toHaveBeenCalledWith({id});
+        expect(mockRepositoryCliente.remove).toHaveBeenCalledWith(resultadoEsperado);
+        expect(resultado).toEqual(resultadoEsperado);
+    })
+  })
 });
