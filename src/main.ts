fba367cd89b533cca22ada185391a2d675021d3a
NicolasArtemio <nicolasartemiodume@gmail.com>
2025-10-28 20:28:12 -0300

    feat
    cors

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilitar CORS
-  app.enableCors({
-    origin: ['http://localhost:5173'],
-    credentials: true,
-  });
   app.setGlobalPrefix('api/v1');
 
   app.enableCors({
