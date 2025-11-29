1996ceecd60b0e9a301e969590c572c97c1c9dc0
NicolasArtemio <nicolasartemiodume@gmail.com>
2025-11-29 16:23:36 -0300

    feat(controller mp)
    -update controller save build

--- src/mercadopago/mercadopago.module.ts
+++ src/mercadopago/mercadopago.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MercadoPagoController } from './mercadopago.controller';
 import { MessageModule } from '../message/message.module';
+import { VentasModule } from 'src/ventas/ventas.module';
 @Module({
-  imports: [MessageModule],
+  imports: [MessageModule, VentasModule],
   controllers: [MercadoPagoController],
 })
 export class MercadoPagoModule {}
