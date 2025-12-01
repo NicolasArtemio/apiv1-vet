2089fb16d95a6ac61aea89f95f269221d6fa0578
NicolasArtemio <nicolasartemiodume@gmail.com>
2025-12-01 12:49:36 -0300

    feat
    update module

--- src/turno/turno.module.ts
+++ src/turno/turno.module.ts
@@ -4,10 +4,11 @@ import { TurnoController } from './turno.controller';
 import { Turno } from './entities/turno.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotificacionesModule } from 'src/notificaciones/notificaciones.module';
+import { Mascota } from 'src/mascotas/entities/mascota.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Turno]),
+    TypeOrmModule.forFeature([Turno, Mascota]),
     forwardRef(() => NotificacionesModule),
   ],
   controllers: [TurnoController],
